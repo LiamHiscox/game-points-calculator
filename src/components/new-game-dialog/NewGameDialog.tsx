8a890e5d716ef9e153d3da9c2703b2d8bc473452
Liam Hiscox <liamhiscox@gmx.at>
2021-09-27 20:30:28 +0200

    new is full screen now

--- src/components/new-game-dialog/NewGameDialog.tsx
+++ src/components/new-game-dialog/NewGameDialog.tsx
@@ -1,8 +1,8 @@
 import './NewGameDialog.scss';
 import {
+  AppBar,
   Button,
   Dialog,
-  Divider,
   Fade,
   FormControl,
   IconButton,
@@ -12,12 +12,13 @@ import {
   ListItem,
   OutlinedInput,
   TextField,
+  Toolbar,
   Typography
 } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import AddIcon from "@material-ui/icons/Add";
 import CloseIcon from '@material-ui/icons/Close';
-import {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {v4 as uuidv4} from "uuid";
 import {GameModel} from "../../models/game.model";
 
@@ -30,14 +31,16 @@ interface NewGameDialogProps {
 
 export function NewGameDialog({open, game, onClose, onSubmit}: NewGameDialogProps) {
   const [newGame, setNewGame] = useState<GameModel>({id: '', timestamp: new Date(), name: '', players: []});
+  const [focus, setFocus] = useState<boolean>(false);
+  const inputEl = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-      open && setNewGame({
-        id: uuidv4(),
-        timestamp: new Date(),
-        name: game.name,
-        players: game.players.map(player => ({...player, points: [null]}))
-      });
+    open && setNewGame({
+      id: uuidv4(),
+      timestamp: new Date(),
+      name: game.name,
+      players: game.players.map(player => ({...player, points: [null]}))
+    });
   }, [game, open]);
 
   const handleDelete = (index: number) => {
@@ -45,6 +48,11 @@ export function NewGameDialog({open, game, onClose, onSubmit}: NewGameDialogProp
       ...newGame,
       players: newGame.players.filter((_, i) => i !== index)
     });
+  }
+
+  const handleClear = () => {
+    inputEl?.current?.focus();
+    setNewGame({...newGame, name: ''});
   }
 
   const handleGameNameChange = (newName: string) => {
@@ -62,6 +70,7 @@ export function NewGameDialog({open, game, onClose, onSubmit}: NewGameDialogProp
   }
 
   const addPlayer = () => {
+    setFocus(true);
     setNewGame({
       ...newGame,
       players: newGame.players.concat({id: uuidv4(), name: '', points: [null]})
@@ -75,55 +84,65 @@ export function NewGameDialog({open, game, onClose, onSubmit}: NewGameDialogProp
   }
 
   return (
-    <Dialog fullWidth
+    <Dialog fullScreen
             open={open}
             onClose={onClose}>
-      <List>
-        <ListItem>
-          <FormControl variant="outlined" className="player-name-form-control">
-            <InputLabel> Game Name </InputLabel>
-            <OutlinedInput
-              type="text"
-              value={newGame.name}
-              error={!newGame.name}
-              onChange={e => handleGameNameChange(e.target.value)}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton onClick={() => handleGameNameChange('')}>
-                    <CloseIcon color="primary"/>
-                  </IconButton>
-                </InputAdornment>
-              }
-              labelWidth={93}
-            />
-          </FormControl>
-        </ListItem>
-        <ListItem>
-          <Typography style={{fontWeight: "bold"}}> Players </Typography>
-        </ListItem>
-        {newGame.players.map((player, index) => (
-          <Fade key={index} in>
-            <ListItem>
-              <TextField label="Player Name" variant="outlined"
-                         value={player.name}
-                         style={{flex: "1"}}
-                         onChange={(e) => handleNameChange(e.target.value, index)}
-                         error={!player.name}
-              />
-              <IconButton onClick={() => handleDelete(index)}>
-                <DeleteIcon color="primary"/>
-              </IconButton>
-            </ListItem>
-          </Fade>
-        ))}
-        <ListItem style={{justifyContent: "center"}}>
-          <IconButton onClick={addPlayer}>
-            <AddIcon color="primary"/>
+      <AppBar position="static">
+        <Toolbar className="new-game-toolbar">
+          <IconButton onClick={onClose} color="inherit">
+            <CloseIcon/>
           </IconButton>
-        </ListItem>
-      </List>
-      <Divider/>
+        </Toolbar>
+      </AppBar>
+      <div className="new-game-list">
+        <List>
+          <ListItem>
+            <FormControl variant="outlined" className="player-name-form-control">
+              <InputLabel> Game Name </InputLabel>
+              <OutlinedInput type="text"
+                             inputRef={inputEl}
+                             value={newGame.name}
+                             error={!newGame.name}
+                             onChange={e => handleGameNameChange(e.target.value)}
+                             endAdornment={
+                               <InputAdornment position="end">
+                                 <IconButton onClick={handleClear}>
+                                   <CloseIcon color="primary"/>
+                                 </IconButton>
+                               </InputAdornment>
+                             }
+                             labelWidth={93}
+              />
+            </FormControl>
+          </ListItem>
+          <ListItem>
+            <Typography style={{fontWeight: "bold"}}> Players </Typography>
+          </ListItem>
+          {newGame.players.map((player, index, players) => (
+            <Fade key={index} in>
+              <ListItem>
+                <TextField label="Player Name" variant="outlined"
+                           autoFocus={focus && index === players.length - 1}
+                           value={player.name}
+                           style={{flex: "1"}}
+                           onChange={(e) => handleNameChange(e.target.value, index)}
+                           error={!player.name}
+                />
+                <IconButton onClick={() => handleDelete(index)}>
+                  <DeleteIcon color="primary"/>
+                </IconButton>
+              </ListItem>
+            </Fade>
+          ))}
+          <ListItem style={{justifyContent: "center"}}>
+            <IconButton onClick={addPlayer}>
+              <AddIcon color="primary"/>
+            </IconButton>
+          </ListItem>
+        </List>
+      </div>
       <Button color="primary"
+              variant="contained"
               onClick={() => onSubmit(newGame)}
               disabled={canSubmit()}
       >
