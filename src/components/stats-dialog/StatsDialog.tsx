1f8c09506d2afdf1732628add4c11db4e0661391
liamh <Liam1997+>
2022-09-22 00:21:46 +0200

    fix scroll issue

--- src/components/stats-dialog/StatsDialog.tsx
+++ src/components/stats-dialog/StatsDialog.tsx
@@ -35,63 +35,67 @@ export function StatsDialog({open, players, onClose}: StatsDialogProps) {
   const [max, setMax] = useState<number>(0);
 
   useEffect(() => {
-    const minMax = players.reduce((acc: MinMaxPointsModel[], player) => {
-      const points = player.points.reduce((total: number[], p) => typeof p?.points === 'number' ? total.concat(p?.points) : total, []);
-      const min = points.reduce((min, cur) => cur < min ? cur : min, points[0] || 0);
-      const max = points.reduce((max, cur) => cur > max ? cur : max, points[0] || 0);
-      return acc.concat({name: player.name, min, max});
-    }, []);
-    const min = minMax.reduce((min, cur) => cur.min < min ? cur.min : min, minMax[0]?.min || 0);
-    const max = minMax.reduce((max, cur) => cur.max > max ? cur.max : max, minMax[0]?.max || 0);
-
-    setMin(min);
-    setMax(max);
-    setMinMaxData(minMax);
-  }, [players]);
+    if (open) {
+      const minMax = players.reduce((acc: MinMaxPointsModel[], player) => {
+        const points = player.points.reduce((total: number[], p) => typeof p?.points === 'number' ? total.concat(p?.points) : total, []);
+        const min = points.reduce((min, cur) => cur < min ? cur : min, points[0] || 0);
+        const max = points.reduce((max, cur) => cur > max ? cur : max, points[0] || 0);
+        return acc.concat({name: player.name, min, max});
+      }, []);
+      const min = minMax.reduce((min, cur) => cur.min < min ? cur.min : min, minMax[0]?.min || 0);
+      const max = minMax.reduce((max, cur) => cur.max > max ? cur.max : max, minMax[0]?.max || 0);
+  
+      setMin(min);
+      setMax(max);
+      setMinMaxData(minMax);
+    }
+  }, [players, open]);
 
   useEffect(() => {
-    const newPlayers = players.map<FilteredPlayerModel>(player => {
-      const points = player.points.reduce((acc: number[], cur) => {
-        if (typeof cur?.points !== 'number') {
-          return acc;
-        }
-        if (acc.length === 0) {
-          return acc.concat(cur.points);
-        }
-        const lastSum = acc[acc.length - 1];
-        return acc.concat(cur.points + lastSum);
-      }, []);
-      return {...player, points};
-    });
-
-    const maxRounds = newPlayers.reduce((max, cur) => cur.points.length > max ? cur.points.length : max, 0);
-
-    const paddedPlayers = newPlayers.map(player => {
-      const length = player.points.length;
-      if (length < maxRounds) {
-        const diff = maxRounds - length;
-        const lastPoints = length > 0 ? player.points[length - 1] : 0;
-        const diffArray = new Array(diff).fill(lastPoints);
-        const padded = player.points.concat(...diffArray);
-        return {...player, points: padded};
-      }
-      return player;
-    });
-
-    const newData = new Array(maxRounds)
-      .fill(null)
-      .map<PointsDataModel>((_, index) => {
-        const playerPoints = paddedPlayers.reduce((acc: {[name: string]: number}, cur) => ({...acc, [cur.name]: cur.points[index]}), {});
-        return {
-          round: index+1,
-          ...playerPoints
-        };
+    if (open) {
+      const newPlayers = players.map<FilteredPlayerModel>(player => {
+        const points = player.points.reduce((acc: number[], cur) => {
+          if (typeof cur?.points !== 'number') {
+            return acc;
+          }
+          if (acc.length === 0) {
+            return acc.concat(cur.points);
+          }
+          const lastSum = acc[acc.length - 1];
+          return acc.concat(cur.points + lastSum);
+        }, []);
+        return {...player, points};
       });
+
+      const maxRounds = newPlayers.reduce((max, cur) => cur.points.length > max ? cur.points.length : max, 0);
+
+      const paddedPlayers = newPlayers.map(player => {
+        const length = player.points.length;
+        if (length < maxRounds) {
+          const diff = maxRounds - length;
+          const lastPoints = length > 0 ? player.points[length - 1] : 0;
+          const diffArray = new Array(diff).fill(lastPoints);
+          const padded = player.points.concat(...diffArray);
+          return {...player, points: padded};
+        }
+        return player;
+      });
+
+      const newData = new Array(maxRounds)
+        .fill(null)
+        .map<PointsDataModel>((_, index) => {
+          const playerPoints = paddedPlayers.reduce((acc: {[name: string]: number}, cur) => ({...acc, [cur.name]: cur.points[index]}), {});
+          return {
+            round: index+1,
+            ...playerPoints
+          };
+        });
 
       console.log({newData, paddedPlayers, maxRounds, newPlayers, players});
 
       setData(newData);
-  }, [players]);
+    }
+  }, [players, open]);
 
   return (
     <Dialog open={open}
@@ -113,7 +117,7 @@ export function StatsDialog({open, players, onClose}: StatsDialogProps) {
             </LineChart>
           </ResponsiveContainer>
         </div>
-        <div>
+        <div style={{overflow: 'auto'}}>
           <Table size="small">
             <TableHead>
               <TableRow>
