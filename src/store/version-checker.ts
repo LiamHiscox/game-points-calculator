55a0a63c58eab29a3f59ec489e4a9257de9a31a8
liamh <Liam1997+>
2023-02-09 19:57:37 +0100

    built project

--- src/store/version-checker.ts
+++ src/store/version-checker.ts
@@ -5,8 +5,10 @@ interface PackageJson {
   version: string;
 }
 
+const versionKey = "package-version";
+
 export const checkVersion = async () => {
-  const localVersion = packageJson.version;
+  const localVersion = localStorage.getItem(versionKey);
   try {
     const remotePackageJson = await axios.get<PackageJson>(packageJson.homepage.replace("/build/", "") + "/package.json");
     const remoteVersion = remotePackageJson.data.version;
@@ -14,6 +16,7 @@ export const checkVersion = async () => {
     if (remoteVersion && localVersion !== remoteVersion) {
       const keys = await caches.keys();
       keys.forEach((key) => caches.delete(key));
+      localStorage.setItem(versionKey, remoteVersion);
       console.log(`clearing cache to update game from ${localVersion} to ${remoteVersion}`)
     }
   } catch (e) {}
