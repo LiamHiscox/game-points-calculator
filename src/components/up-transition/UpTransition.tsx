4757d3a1499faa38bd5e9820c9c6d75823367004
liamh <Liam1997+>
2023-04-17 19:53:03 +0200

    created new build

--- src/components/up-transition/UpTransition.tsx
+++ src/components/up-transition/UpTransition.tsx
@@ -3,10 +3,10 @@ import {TransitionProps} from '@mui/material/transitions';
 import {Slide} from '@mui/material';
 
 export const UpTransition = React.forwardRef(function Transition(
-  props: TransitionProps & {
+  {children, ...props}: TransitionProps & {
     children: React.ReactElement;
   },
   ref: React.Ref<unknown>
 ) {
-  return <Slide direction="up" ref={ref} {...props}/>;
+  return <Slide direction="up" ref={ref} children={children} {...props}/>;
 });
