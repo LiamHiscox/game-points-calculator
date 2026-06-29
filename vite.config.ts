88b7ec387c373d90a63759c4d11478ce53d62bae
LiamHiscox <liamhiscox@gmx.at>
2026-06-29 19:22:52 +0200

    update paths and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/game-points-calculator/dist/',
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
     injectRegister: false,
